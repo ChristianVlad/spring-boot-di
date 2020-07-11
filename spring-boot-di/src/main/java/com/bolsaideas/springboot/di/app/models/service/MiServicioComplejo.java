ac85efa05cfd50e64ea5cd650c8aaa07353964c4
ChristianVlad <Christian.valladarezz@gmail.com>
2020-07-11 00:54:27 -0600

    Adding new class

---
+++ spring-boot-di/src/main/java/com/bolsaideas/springboot/di/app/models/service/MiServicioComplejo.java
@@ -0,0 +1,15 @@
+package com.bolsaideas.springboot.di.app.models.service;
+
+import org.springframework.stereotype.Component;
+
+
+@Component("miServicioComplejo")
+public class MiServicioComplejo implements IService {
+	
+	@Override
+	public String operacion() {
+		return "ejecutando un proceso inmportante o algo asi...";
+	}
+	
+
+}
